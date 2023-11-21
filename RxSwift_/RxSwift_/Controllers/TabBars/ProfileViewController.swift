f4dff0c1db29027ca5af3cdc9a87497dfe088ad9
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-11-21 08:12:39 +0100

    updated

--- RxSwift_/RxSwift_/Controllers/TabBars/ProfileViewController.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/ProfileViewController.swift
@@ -9,30 +9,32 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    let informationLabel = Label(label: "Information", textColor: .black, font: UIFont.systemFont(ofSize: 24, weight: .semibold))
+    
     lazy var profileImage: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.backgroundColor = .gray
-        image.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        image.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        image.layer.cornerRadius = 25
+        image.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        image.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        image.layer.cornerRadius = 35
         return image
     }()
     
-    let profileName = Label(label: "John doe", textColor: .black, font: UIFont.systemFont(ofSize: 17, weight: .medium))
+    let profileName = Label(label: "John doe", textColor: .black, font: UIFont.systemFont(ofSize: 24, weight: .semibold))
     
-    let profileEmail = Label(label: "johndoe@gmail.com", textColor: .gray, font: UIFont.systemFont(ofSize: 10, weight: .regular))
+    let profileEmail = Label(label: "johndoe@gmail.com", textColor: .gray, font: UIFont.systemFont(ofSize: 16, weight: .regular))
     
-    let profilePhone = Label(label: "+23480", textColor: .gray, font: UIFont.systemFont(ofSize: 10, weight: .regular))
+    let profilePhone = Label(label: "+23480", textColor: .gray, font: UIFont.systemFont(ofSize: 16, weight: .regular))
     
-    let profileAddress = Label(label: "No 16 john doe street", textColor: .gray, font: UIFont.systemFont(ofSize: 10, weight: .regular))
+    let profileAddress = Label(label: "No 16 john doe street", textColor: .gray, font: UIFont.systemFont(ofSize: 16, weight: .regular))
     
     lazy var stackView: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [profileName, profileEmail, profilePhone, profileAddress])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
-        stack.spacing = 5
-        stack.alignment = .center
+        stack.spacing = 10
+        stack.alignment = .fill
         return stack
     }()
     
@@ -50,17 +52,21 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
-        view.backgroundColor = .gray
+        view.backgroundColor = #colorLiteral(red: 0.9436392188, green: 0.9436392188, blue: 0.9436392188, alpha: 1)
         setupViews()
     }
     
     func setupViews() {
+        view.addSubview(informationLabel)
         view.addSubview(profileStackView)
         NSLayoutConstraint.activate([
-            profileStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            profileStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            profileStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
-            profileStackView.heightAnchor.constraint(equalToConstant: 200)
+            informationLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            informationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            profileStackView.topAnchor.constraint(equalTo: informationLabel.bottomAnchor, constant: 10),
+            profileStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
+            profileStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            profileStackView.heightAnchor.constraint(equalToConstant: 150)
         ])
     }
 }
