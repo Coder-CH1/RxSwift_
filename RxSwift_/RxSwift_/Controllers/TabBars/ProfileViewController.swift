fd784fd68d93c8e8144570c2551b481f3004a3f2
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-11-23 14:33:44 +0100

    updated

--- RxSwift_/RxSwift_/Controllers/TabBars/ProfileViewController.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/ProfileViewController.swift
@@ -11,6 +11,10 @@ class ProfileViewController: UIViewController {
     
     let myProfileLabel = Label(label: "My Profile", textColor: .black, font: UIFont.systemFont(ofSize: 30, weight: .semibold))
     
+    let personalDetailsLabel = Label(label: "Personal details", textColor: .black, font: UIFont.systemFont(ofSize: 17, weight: .semibold))
+    
+    let changeButton = Button(image: UIImage(systemName: ""), label: "change", btnTitleColor: #colorLiteral(red: 0.9803921569, green: 0.2901960784, blue: 0.04705882353, alpha: 1), backgroundColor: .clear, radius: 0, imageColor: .clear)
+    
     lazy var profileView: UIView = {
         let profileView = UIView()
         profileView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,15 +33,23 @@ class ProfileViewController: UIViewController {
     func setupViews() {
         view.addSubview(myProfileLabel)
         view.addSubview(profileView)
+        view.addSubview(personalDetailsLabel)
+        view.addSubview(changeButton)
         
         NSLayoutConstraint.activate([
             myProfileLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            myProfileLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            myProfileLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45),
             
-            profileView.topAnchor.constraint(equalTo: myProfileLabel.bottomAnchor, constant: 20),
+            personalDetailsLabel.topAnchor.constraint(equalTo: myProfileLabel.bottomAnchor, constant: 20),
+            personalDetailsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            
+            changeButton.topAnchor.constraint(equalTo: myProfileLabel.bottomAnchor, constant: 15),
+            changeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            profileView.topAnchor.constraint(equalTo: personalDetailsLabel.bottomAnchor, constant: 5),
             profileView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
             profileView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
-            profileView.heightAnchor.constraint(equalToConstant: 133),
+            profileView.heightAnchor.constraint(equalToConstant: 197),
         ])
     }
 }
