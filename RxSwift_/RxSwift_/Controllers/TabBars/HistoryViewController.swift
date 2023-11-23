e0684977635205c6262f0566c9179144615d6599
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-11-23 07:01:49 +0100

    updated

--- RxSwift_/RxSwift_/Controllers/TabBars/HistoryViewController.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/HistoryViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class HistoryViewController: UIViewController {
+    
+    let orderingButton = Button(image: UIImage(systemName: ""), label: "Start ordering", btnTitleColor: .white, backgroundColor: #colorLiteral(red: 0.9803921569, green: 0.2901960784, blue: 0.04705882353, alpha: 1), radius: 30, imageColor: .clear)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "History"
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.addSubview(orderingButton)
+        NSLayoutConstraint.activate([
+            orderingButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80),
+            orderingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            orderingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            orderingButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
 }
