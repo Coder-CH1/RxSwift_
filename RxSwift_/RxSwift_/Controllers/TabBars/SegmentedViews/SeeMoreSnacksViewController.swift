84bfa70a80aa03100b2686cca045f6f02549f765
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-12-11 15:52:19 +0100

    updated

--- RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/SeeMoreSnacksViewController.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/SeeMoreSnacksViewController.swift
@@ -9,7 +9,28 @@ import UIKit
 
 class SeeMoreSnacksViewController: UIViewController {
 
+    var num: Int = 0
+    
+    var titleLabel = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        configureTitleLabel()
+    }
+    
+    func configureTitleLabel() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = .black
+        titleLabel.text = "found \(num) result"
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+    func setupViews() {
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
     }
 }
