2fd152834ee8807cb9ae8c3460141ff9286e0736
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-12-14 16:42:21 +0100

    updated

--- RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/FoodsDetailsViewController.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/FoodsDetailsViewController.swift
@@ -7,15 +7,35 @@
 
 import UIKit
 
-class FoodsDetailsViewController: UIViewController {
+class FoodsDetailsViewController: UIViewController, UIScrollViewDelegate {
+    
+    let scrollView = UIScrollView()
     
     let pageControl = UIPageControl()
     
+    let imageNames = ["fireIcon", "appLogo", "fireIcon", "appLogo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        configureScrollView()
         configurePageControl()
         pageControlSelectedAction()
+    }
+    
+    func configureScrollView() {
+        scrollView.frame = view.bounds
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(imageNames.count), height: 100)
+        
+        for (index, imageName) in imageNames.enumerated() {
+            let imageView = UIImageView(frame: CGRect(x: view.bounds.width * CGFloat(index), y: 50, width: view.bounds.width, height: 100))
+            imageView.image = UIImage(named: imageName)
+            imageView.contentMode = .scaleAspectFit
+            scrollView.addSubview(imageView)
+        }
+        view.addSubview(scrollView)
     }
     
     func configurePageControl() {
@@ -24,6 +44,7 @@ class FoodsDetailsViewController: UIViewController {
         pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.9803921569, green: 0.2901960784, blue: 0.04705882353, alpha: 1)
         pageControl.pageIndicatorTintColor = .lightGray
         pageControl.frame = CGRect(x: 0, y: 320, width: view.frame.size.width, height: 50)
+        view.addSubview(pageControl)
     }
     
     func pageControlSelectedAction() {
@@ -34,10 +55,13 @@ class FoodsDetailsViewController: UIViewController {
     }
     
     func pageControlTapped(_ sender: UIPageControl) {
-        //let foodsSection: Int = sender.currentPage
+        let currentPage = pageControl.currentPage
+        let xOffset = view.bounds.width * CGFloat(currentPage)
+        scrollView.setContentOffset(CGPoint(x: xOffset, y: -100), animated: false)
     }
     
-    func setupViews() {
-        view.addSubview(pageControl)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.bounds.width)
+        pageControl.currentPage = Int(pageIndex)
     }
 }
