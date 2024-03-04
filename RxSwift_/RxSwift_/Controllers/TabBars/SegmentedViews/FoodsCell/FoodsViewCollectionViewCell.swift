210f845acbe33a3ff15cc946f10d106a6222bbc8
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-03-04 11:16:35 +0100

    added

--- RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/FoodsCell/FoodsViewCollectionViewCell.swift
+++ RxSwift_/RxSwift_/Controllers/TabBars/SegmentedViews/FoodsCell/FoodsViewCollectionViewCell.swift
@@ -8,5 +8,9 @@
 import UIKit
 
 class FoodsViewCollectionViewCell: UICollectionViewCell {
+    let img = CustomImageView(image: UIImage(named: ""))
     
+    let lbl1 = Label(label: "", textColor: .black, font: UIFont.systemFont(ofSize: 48, weight: .semibold))
+    
+    let lbl2 = Label(label: "", textColor: .systemOrange, font: UIFont.systemFont(ofSize: 48, weight: .semibold))
 }
